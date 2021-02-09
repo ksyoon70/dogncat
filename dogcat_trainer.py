4a1218b3dc230704a07ca34b481ae204716cbfc1
윤경섭 <ksyoon70@gmail.com>
2021-02-10 00:09:05 +0900

    수정 [M006]
    로직 추가

--- dogcat_trainer.py
+++ dogcat_trainer.py
@@ -11,6 +11,8 @@ from keras import layers
 from keras import models
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
+import matplotlib.pyplot as plt
 
 
 original_dataset_dir = './datasets/training_set'
@@ -140,6 +142,7 @@ model.add(layers.MaxPooling2D((2,2)))
 model.add(layers.Conv2D(128,(3,3),activation='relu'))
 model.add(layers.MaxPooling2D((2,2)))
 model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(512,activation='relu'))
 model.add(layers.Dense(1,activation='sigmoid'))
 
@@ -147,7 +150,48 @@ model.add(layers.Dense(1,activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4),metrics=['acc'])
 
-train_datagen = ImageDataGenerator(rescale=1./255)
+"""
+# 샘플영상을 표시하여 봄
+datagen = ImageDataGenerator(rotation_range=20,
+                             width_shift_range=0.1,
+                             height_shift_range=0.1,
+                             shear_range=0.1,
+                             zoom_range=0.1,
+                             horizontal_flip=True,
+                             fill_mode='nearest')
+
+fnames = sorted([os.path.join(train_cats_dir,fname) for fname in os.listdir(train_cats_dir)])
+
+img_path = fnames[3]  #증식할 이미지 선택합니다.
+
+img = image.load_img(img_path,target_size=(150,150))
+
+x = image.img_to_array(img)
+x = x.reshape((1,) + x.shape)
+
+i = 0
+
+for batch in datagen.flow(x, batch_size=1):
+    plt.figure(i)
+    imgplot = plt.imshow(image.array_to_img(batch[0]))
+    i += 1
+    
+    if i % 4 == 0:
+        break
+
+plt.show()
+"""
+
+train_datagen = ImageDataGenerator(
+                            rescale=1./255,
+                            rotation_range=40,
+                             width_shift_range=0.2,
+                             height_shift_range=0.2,
+                             shear_range=0.2,
+                             zoom_range=0.2,
+                             horizontal_flip=True)
+
+#train_datagen = ImageDataGenerator(rescale=1./255)
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(train_dir,
@@ -167,4 +211,38 @@ history = model.fit_generator(train_generator,
                               validation_data=validation_generator,
                               validation_steps=50)
 
-model.save('cats_and_dogs_small_1.h5')
+model.save('cats_and_dogs_small_2.h5')
+
+
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+
+plt.plot(epochs, acc, 'bo', label ='Training acc')
+plt.plot(epochs, val_acc, 'b', label ='Validation acc')
+
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label ='Training loss')
+plt.plot(epochs, val_loss, 'b', label ='Validation loss')
+
+plt.title('Training and validation loss')
+plt.legend()
+plt.figure()
+
+plt.show()
+
+
+
+
+
+
+
+
