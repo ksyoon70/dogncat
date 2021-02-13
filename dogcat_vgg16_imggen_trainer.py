9aabfa318ea22e35c04959b82c1313b2515b1a87
윤경섭 <ksyoon70@gmail.com>
2021-02-13 18:55:45 +0900

    수정 [M009]
    - tensorfow dataset으로 VGG16 개, 고양이 시험
    - ImageDataGenerator로 VGG16 개, 고양이 시험

--- dogcat_vgg16_imggen_trainer.py
+++ dogcat_vgg16_imggen_trainer.py
@@ -27,7 +27,7 @@ categories = ["cat","dog"]
 #이미지 크기 조정 크기
 IMG_SIZE = 224
 #배치 싸이즈
-BATCH_SIZE = 4
+BATCH_SIZE = 20
 
 training_data = []
 training_file_info = []
@@ -153,149 +153,30 @@ if not len(os.listdir(test_dogs_dir)):
 else:
     print('테스트용 강아지 이미지 갯수:',len(os.listdir(test_dogs_dir)))
     
+train_datagen = ImageDataGenerator(
+                            rescale=1./255,
+                            rotation_range=40,
+                             width_shift_range=0.2,
+                             height_shift_range=0.2,
+                             shear_range=0.2,
+                             zoom_range=0.2,
+                             horizontal_flip=True)
 
-def preload_file_name(max_file_num,categories,dirs,file_info):
-    for categorie in categories:
-        path = os.path.join(dirs,categorie +'s')
-        class_num = categories.index(categorie)
-        ctfiles = os.listdir(path)
-        ctotal_file_num = np.minimum(max_file_num,len(os.listdir(path)))
-        files = [ctfiles[i] for i in range(0,ctotal_file_num)]
-        for file in files:
-            file_info.append([os.path.join(path,file),class_num])
+#train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
 
-#------------- 영상 복사 여기까지 --------------
-
-
-def _create_training_data(filename,label):
- 
-    img_array = cv2.imread(str(filename))
-    resize_array = cv2.resize(img_array,(224,224))  #크기ㅣ 변경함.
-    #training_data.append([resize_array, label])  # 0 ~ 255 사이의 데이터
-    np_image_data = np.asarray(resize_array)
-    img_array = tf.convert_to_tensor(np_image_data, dtype=tf.float32) / 255.
-    
-    return (np_image_data, label)
-
-def _create_training_data1(filename,label):
-    img_array = tf.io.read_file(filename)
-    img_array = tf.image.decode_jpeg(img_array,channels=3)
-    img_array = tf.image.convert_image_dtype(img_array, tf.float32) 
-    resize_array = tf.image.resize(img_array,(IMG_SIZE,IMG_SIZE))
-    #resize_array /= 255.
-    #resize_array = tf.reshape(resize_array, [-1, IMG_SIZE, IMG_SIZE, 3])
-    return (resize_array,label)
-
-    
-    
-
-preload_file_name(1000,categories,train_dir,training_file_info)
-
-random.shuffle(training_file_info)
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    target_size=(IMG_SIZE,IMG_SIZE),
+                                                    batch_size=BATCH_SIZE,
+                                                    class_mode='binary')
 
 
-preload_file_name(500,categories,validation_dir,validation_file_info)
-
-random.shuffle(validation_file_info)
-
- #파일이름과 레이블 분리
-filenames =[]
-labels =[]
-
-for info in training_file_info:
-    filenames.append(info[0])
-    labels.append(info[1])
-
-vfilenames =[]
-vlabels =[]
-
-for info in validation_file_info:
-    vfilenames.append(info[0])
-    vlabels.append(info[1])
+validation_generator = test_datagen.flow_from_directory(validation_dir,
+                                                    target_size=(IMG_SIZE,IMG_SIZE),
+                                                    batch_size=BATCH_SIZE,
+                                                    class_mode='binary')
 
 
-# step 3: parse every image in the dataset using `map`
-"""
-def _parse_function(filename, label):
-    image_reader = tf.WholeFileReader()
-    _, image_string = image_reader.read(filename)
-    #image_string = tf.read_file(filename)
-    image_decoded = tf.image.decode_jpeg(image_string, channels=3)
-    image = tf.cast(image_decoded, tf.float32)
-    resized_image = tf.image.resize_images(image, [IMG_SIZE, IMG_SIZE])
-    return resized_image, label
-"""
-
-#x = create_training_data(filenames[0],labels[0])
-
-
-
-
-
-training_data = tf.data.Dataset.from_tensor_slices((filenames, labels))
-
-#_create_training_data1(filenames[0],labels[0]) #test
-
-training_data = training_data.map(_create_training_data1)
-
-
-training_data = training_data.batch(BATCH_SIZE)
-
-
-
-# 데이터 증식
-"""
-datagen = ImageDataGenerator(
-    featurewise_center=True,
-    featurewise_std_normalization=True,
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.1,
-    zoom_range=0.1,
-    horizontal_flip=True)
-
-"""
-
-# compute quantities required for featurewise normalization
-# (std, mean, and principal components if ZCA whitening is applied)
-
-#print(str(type(training_data)))  #데이터 type 확인 
-
-"""
-x = training_data.map(lambda x, y: x)
-y = training_data.map(lambda x, y: y)
-    
-datagen.fit(x)
-"""
-
-
-validation_data = tf.data.Dataset.from_tensor_slices((vfilenames, vlabels))
-
-#_create_training_data1(vfilenames[0],vlabels[0]) #test
-
-validation_data = validation_data.map(_create_training_data1)
-
-validation_data = validation_data.batch(BATCH_SIZE)
-
-
-# step 4: create iterator and final input tensor
-"""
-for data, label in dataset.take(1):
-    proto_tensor = tf.make_tensor_proto(data[0,0])
-    array = tf.make_ndarray(proto_tensor)
-    plt.imshow(array)
-"""
-
-
-#create_training_data(1000)
-
-#random.shuffle(training_data)
-"""
-for feature, label in training_data:
-    x.append(feature)
-    y.append(label)
-"""
   
 conv_base = VGG16(weights='imagenet',
                   include_top = False,
@@ -319,12 +200,11 @@ model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4),
 
 #model.summary()
 
-history = model.fit(training_data,
-            batch_size = BATCH_SIZE,
-            validation_data=validation_data,
-            validation_steps=50,
-            epochs=10,
-)
+history = model.fit_generator(train_generator,
+                              steps_per_epoch=50,
+                              epochs=30,
+                              validation_data=validation_generator,
+                              validation_steps=25)
  
     
 model.save('cats_and_dogs_small_3.h5')
